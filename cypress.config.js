a125e673ae54f472d5727f2f89bc2b3163627af7
Julio Gama <juliogamanetto@outlook.com>
2025-12-15 21:32:21 -0300

    Alguns ajustes de diretórios

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,26 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost/',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
   },
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  fixturesFolder: false,
+  video: false,
+  screenshotOnRunFailure: true,
+  retries: {
+    runMode: 2,   // headless / CI
+    openMode: 0   // modo interativo
+  },
+
+  env: {
+    environment: 'local'
+  }
 });
